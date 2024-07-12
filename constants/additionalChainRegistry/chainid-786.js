1a55fe2daab07345ef80bf971f5d21cc39039096
ADNAN ALI <118028387+FAStstorebitbankuk@users.noreply.github.com>
2024-07-12 23:45:35 +0000

    add chain JB-mainnet

---
+++ constants/additionalChainRegistry/chainid-786.js
@@ -0,0 +1,25 @@
+export const data = {
+    "name": "JB-mainnet",
+    "chain": "JB",
+    "icon": "eth",
+    "rpc": [
+      "https://virtual.mainnet.rpc.tenderly.co/f06c7662-a76b-4708-ba53-2954cf6011d0"
+    ],
+    "features": [{ "name": "EIP155" }],
+    "faucets": [],
+    "nativeCurrency": {
+      "name": "ADNANJB",
+      "symbol": "AJB",
+      "decimals": 18
+    },
+    "infoURL": "https://adnanjb.com",
+    "shortName": "ethw",
+    "chainId": 786,
+    "networkId": 786,
+    "explorers": [
+      {
+        "name": "ADNANJB",
+        "url": "https://www.adnanjb.com/AJB/"
+      }
+    ]
+  }
